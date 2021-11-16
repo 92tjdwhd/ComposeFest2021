9c6fbeaf69c97580cbda6b61f0f056b0e7f1da9f
92tjdwhd <92tjdwhd@naver.com>
2021-11-16 23:27:57 +0900

    feature - Styling and theming your app

--- week 1-Jetpack Compose basics/basicsCodelab2/app/src/main/java/com/tjdwhd92/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/basicsCodelab2/app/src/main/java/com/tjdwhd92/basicscodelab2/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.tjdwhd92.basicscodelab2
 
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Expand
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tjdwhd92.basicscodelab2.ui.theme.BasicsCodelab2Theme
@@ -28,22 +42,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState( if (expanded) 48.dp else 0.dp, animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,stiffness = Spring.StiffnessLow))
 
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "show less" else "Show more")
+           IconButton(onClick = { expanded = !expanded }) {
+               Icon(imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,contentDescription = if (expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
             }
         }
 
@@ -63,7 +77,7 @@ fun Greeting(name: String) {
 @Composable
 fun MyApp() {
 
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
@@ -73,16 +87,17 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000){"$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items (items = names) {name ->
             Greeting(name = name)
         }
     }
 }
+
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface {
         Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
             Text(text = "Welcome to the Basics Codelab!")
@@ -94,7 +109,9 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 
-@Preview(showBackground = true, widthDp = 320)
+
+
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES,name = "DefaultPreviewDark")
 @Composable
 fun DefaultPreview() {
     BasicsCodelab2Theme {
